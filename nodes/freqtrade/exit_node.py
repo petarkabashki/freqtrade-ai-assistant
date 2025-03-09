8688a6f91e6778420b5043c34a2c012353483ffe
Petar Kabashki <peter.kabashky@gmail.com>
2025-03-09 23:54:28 +0000

    feat: Add exit node for freqtrade download assistant

---
+++ nodes/freqtrade/exit_node.py
@@ -0,0 +1,9 @@
+from util.pocketflow import Node
+
+class ExitNode(Node):
+    def exec(self, prep_res, shared):
+        print("\nThanks for using the Freqtrade Download Assistant!")
+        return None
+
+    def post(self, shared, prep_res, exec_res):
+        return {}
