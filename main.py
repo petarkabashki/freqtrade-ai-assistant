4474da701103363051dd358870ef95800e437698
Petar Kabashki <peter.kabashky@gmail.com>
2025-03-09 01:22:59 +0000

    refactor: Refactor import path for MainFlowNode

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from freq_assistant.nodes.main_flow.main_flow_node import MainFlowNode
+from nodes.main_flow.main_flow_node import MainFlowNode
 from lib.pocketflow import Flow
 
 if __name__ == '__main__':
     main_flow_node = MainFlowNode()
     shared_data = {}
     main_flow = main_flow_node.run(shared_data)
-    main_flow.run(shared_data)
+    # main_flow.run(shared_data)
