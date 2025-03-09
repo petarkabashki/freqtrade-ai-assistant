fa3261a1e7a6964547c28f710c1e2ef17e1b08b0
Petar Kabashki <peter.kabashky@gmail.com>
2025-03-09 02:55:02 +0000

    feat: Add freqtrade flow definition

---
+++ nodes/freqtrade/flow.py
@@ -0,0 +1,42 @@
+
+from lib.pocketflow import *
+from .input_node import InputNode
+from .validation_node import ValidationNode
+from .confirmation_node import ConfirmationNode
+from .download_node import DownloadNode
+from .summary_node import SummaryNode
+from .exit_node import ExitNode
+
+def freqtrade_flow():
+    nodes = {
+        "input": InputNode(),
+        "validation": ValidationNode(),
+        "confirmation": ConfirmationNode(),
+        "download": DownloadNode(),
+        "summary": SummaryNode(),
+        "exit": ExitNode(),
+    }
+
+    input_node = nodes["input"]
+    validation_node = nodes["validation"]
+    confirmation_node = nodes["confirmation"]
+    download_node = nodes["download"]
+    summary_node = nodes["summary"]
+    exit_node = nodes["exit"]
+
+    input_node - "validate_input" >> validation_node
+    input_node - "exit" >> exit_node
+
+    validation_node - "confirmation" >> confirmation_node
+    validation_node - "reinput" >> input_node
+
+    confirmation_node - "execute_download" >> download_node
+    confirmation_node - "reinput" >> input_node
+
+    download_node - "summary" >> summary_node
+
+    summary_node - "exit" >> exit_node
+    summary_node - "input" >> input_node
+
+    flow = Flow(start=input_node)
+    return flow
